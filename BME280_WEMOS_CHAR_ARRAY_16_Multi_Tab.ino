d9149e72ce99163e455d49d20ca8b30ba91f6a7f
jlmyra <jlmyra@gmail.com>
2019-11-30 17:48:43 -0500

    Update BME280_WEMOS_CHAR_ARRAY_16_Multi_Tab.ino

--- BME280_WEMOS_CHAR_ARRAY_16_Multi_Tab.ino
+++ BME280_WEMOS_CHAR_ARRAY_16_Multi_Tab.ino
@@ -8,23 +8,20 @@ WiFiClient client; //for Thingspeak
 //********************Location Settings*****************************
 
 //*****BASEMENT WEMOS D1 (myraclegarden.com)
-unsigned long myChannelNumber = 79378;  const char * myWriteAPIKey = "U0RIIBW1U68RE6B9"; String api_key = "414250ebeaabfb8a3a0d3def18efed69";
+unsigned long myChannelNumber = XXXXXX;  const char * myWriteAPIKey = "XXXXXXXXXX"; String api_key = "XXXXXXXXXX";
  
 //******QUONSET WEMOS D1 (mgcquonset.com)
-//unsigned long myChannelNumber = 77784; const char * myWriteAPIKey = "PGIXBEEVDHXX2P7E"; String api_key = "a3b5b8546a3e6c6176d070209992785c"; //DarkSky API
+//unsigned long myChannelNumber = XXXXXX; const char * myWriteAPIKey = "XXXXXXXXXX"; String api_key = "XXXXXXXXXX"; //DarkSky API
 
 //********************Location Settings*****************************
 
 //*********************** WIFI SETTINGS **************************
-//const char *ssid = "DeMEtEr";
-const char *ssid = "DiOnySyS";
-const char *password = "QNX6SKnuCeky";
-
-//const char *ssid = "JLM_iPhone";
-//const char *password = "x7ypavwb9g86b";
+//const char *ssid = "XXXXXXX";
+const char *ssid = "XXXXXXXXXX";
+const char *password = "XXXXXXXXXX";
 
 //const char *ssid = "Maplewood_GC"; //Quonset
-//const char *password = "wru3A@Ur";//Quonset
+//const char *password = "XXXXXXXXXX";//Quonset
 
 //***********************End WIFI SETTINGS **************************
 
